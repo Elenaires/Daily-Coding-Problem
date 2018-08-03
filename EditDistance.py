b68e824d16cdadf23668a86a566ebbb2c2e85061
Lee Huey Charn <hueycharn.lee@gmail.com>
2018-08-02 21:18:32 -0700

    updated with comments

--- EditDistance.py
+++ EditDistance.py
@@ -22,14 +22,26 @@ def editDistance(str1, str2):
 				table[i][j] = i
 				
 			# if the last characters of str1 and str2 are the same
-			# recurse for remaining of the string
-			# fill the table with the diagonal value
+			# ignore last character and add no additional cost
+			# example: 'abbc' to 'acc' last characters are the same
+			# thus cost is what it takes to change 'abb' to 'ac' only
+			# fill the table with the diagonal value (i-1)(j-1) since we
+			# ignore last character
 			elif str1[i-1] == str2[j-1]:
 				table[i][j] = table[i-1][j-1]
 				
 			# if the last characters are different, use min of insert/remove/replace
+			# example: 'abb', 'ac'
+			# 1) table[i][j-i] 'abb' , 'a' : the cost of converting 'abb'
+			# to 'a', plus an additional step to insert 'c' into 'a'
+			# to maken'ac'
+			# 2) table[i-1][j] 'ab', 'ac' : the cost to convert 'ab' to 'ac' plus
+			# an additional step to delete 'b' from 'abb'
+			# 3) table[i-1][j-1] 'ab', 'a' : the cost to convert 'ab' to 'a' plus
+			# an additional step to replace 'b' (last char of 'abb') to 'a' (last
+			# char of 'ac')
 			else:
-				table[i][j] = 1 + min(table[i][j-1], #insert
+				table[i][j] = 1 + min(table[i][j-1], #insert 
 									  table[i-1][j], #remove
 									  table[i-1][j-1]) #replace
 				
